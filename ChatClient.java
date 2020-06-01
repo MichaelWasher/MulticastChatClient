20d106d25bed57d339607deaa92d7f102e351bc4
Michael Washer <michael.washer@icloud.com>
2020-06-01 23:29:11 +1200

    Add Username functionality
    Add username functionality
    Change default message format
    ---Change all Tabs to Spaces---

--- ChatClient.java
+++ ChatClient.java
@@ -16,57 +16,58 @@ import java.net.InetAddress;
 import java.net.UnknownHostException;
 
 class ChatClient{
-	public static boolean checkArgs(String[] args){
-		// Check args are usable or return false
-		boolean success = false;
-		try{
+    public static boolean checkArgs(String[] args){
+        // Check args are usable or return false
+        boolean success = false;
+        try{
 
-			if(args.length != 3)
-				throw new IllegalArgumentException("You must provide 3 arguments");
-			else if(args[0].length() >= 20)
-				throw new IllegalArgumentException("The username value must be less than 20 characters.");
-			else if(Integer.parseInt(args[2]) < 1025)
-				throw new IllegalArgumentException("The provided port number must be greater than 1024.");
-			else if(InetAddress.getByName(args[1]) == null) // Will throw if missing
-				throw new IllegalArgumentException("The IP address provided must be valid");
+            if(args.length != 3)
+                throw new IllegalArgumentException("You must provide 3 arguments");
+            else if(args[0].length() >= 20)
+                throw new IllegalArgumentException("The username value must be less than 20 characters.");
+            else if(Integer.parseInt(args[2]) < 1025)
+                throw new IllegalArgumentException("The provided port number must be greater than 1024.");
+            else if(InetAddress.getByName(args[1]) == null) // Will throw if missing
+                throw new IllegalArgumentException("The IP address provided must be valid");
 
-			success = true;
-		}catch(NumberFormatException e){
-			System.out.println("The port number provided must be a port number.");
-		}catch(UnknownHostException e){
-			System.out.println("The IP address provided must be valid");
-		}catch(IllegalArgumentException e){
-			System.out.println(e.getMessage());
-		}
-		return success;
-	}
+            success = true;
+        }catch(NumberFormatException e){
+            System.out.println("The port number provided must be a port number.");
+        }catch(UnknownHostException e){
+            System.out.println("The IP address provided must be valid");
+        }catch(IllegalArgumentException e){
+            System.out.println(e.getMessage());
+        }
+        return success;
+    }
 
-	public static void printUsage(){
-		System.out.println("java ChatClient <username> <multicast-ip> <port>");
-		System.out.println("EXAMPLE: java ChatClient michaelwasher 239.0.202.1 40202");
-		return;
-	}
+    public static void printUsage(){
+        System.out.println("java ChatClient <username> <multicast-ip> <port>");
+        System.out.println("EXAMPLE: java ChatClient michaelwasher 239.0.202.1 40202");
+        return;
+    }
 
-	// TODO: This shouldn't throw but clean up
-	public static void main(String[] args) throws Exception
-	{
-		// Output usage and check input args
-		if (!checkArgs(args)){
-			printUsage();
-			return;
-		}
-		// TODO: Check for -h / --help
-		
-		// Get IP address 
-		InetAddress multicastAddress = InetAddress.getByName(args[1]);
-		int portNum = Integer.parseInt(args[2]);
-		// TODO: Allow input of portNum... Throw if x < 1024
-		// TODO: Allow users to provide a username within the chat system.
-		// NOTE: Don't check if it's used.
+    // TODO: This shouldn't throw but clean up
+    public static void main(String[] args) throws Exception
+    {
+        // Output usage and check input args
+        if (!checkArgs(args)){
+            printUsage();
+            return;
+        }
+        // TODO: Check for -h / --help
+        
+        // Get IP address 
+        InetAddress multicastAddress = InetAddress.getByName(args[1]);
+        int portNum = Integer.parseInt(args[2]);
+        String username = args[0];
+        // TODO: Allow input of portNum... Throw if x < 1024
+        // TODO: Allow users to provide a username within the chat system.
+        // NOTE: Don't check if it's used.
 
-		Client client = new Client(multicastAddress, portNum);
-		client.start();
-	}
+        Client client = new Client(username, multicastAddress, portNum);
+        client.start();
+    }
 }
 
 // TODO: Add basic tests
--- Client.java
+++ Client.java
@@ -1,127 +1,145 @@
+/**
+ *
+ * @author  Michael Washer
+ * @since   2014
+ * 
+*/
 import java.net.*;
 import java.lang.Thread;
 import java.util.Scanner;
 import java.nio.charset.StandardCharsets;
 import java.io.*;
+import java.time.format.DateTimeFormatter;  
+import java.time.LocalDateTime;  
+import java.lang.IllegalArgumentException;
 
 class Client{
     
     // Properties
-	MulticastSocket ms;
-	int portNum = 40202;
+    MulticastSocket ms;
+    int portNum = 40202;
     InetAddress multicastAddress;
-    
-	ReceiveMessages receiveThread;
+    String messagePrefix = "";
+    String username = "";
 
-	String quittingKey = "#quit";
-    public Client(InetAddress multicastAddress, int portNum) throws IllegalArgumentException 
+    ReceiveMessages receiveThread;
+    
+
+    String quittingKey = "#quit";
+    public Client(String username, InetAddress multicastAddress, int portNum) throws IllegalArgumentException 
     {
         //Set Up Client
         // TODO: perform checks on the variables
-		// TODO: Remove 'contstructor' pass around
+        // TODO: Remove 'contstructor' pass around
         try{
-			//Set Up Client
+            //Set Up Client
             this.multicastAddress = multicastAddress;
             this.portNum = portNum;
             this.ms = new MulticastSocket(portNum);          
-			this.ms.joinGroup(this.multicastAddress);	
-		}catch(Exception e)
-		{
-			e.printStackTrace();
-		}
-	}
+            this.ms.joinGroup(this.multicastAddress);
+            this.username = username;
+            // Set message prefix
+            messagePrefix = String.format("%s(%s)\t",this.username, this.multicastAddress.getHostAddress());
+        }catch(Exception e)
+        {
+            e.printStackTrace();
+            throw new IllegalArgumentException("One of the arguments provided were unable to be processed.");
+        }
+    }
 
-	public void start()
-	{
-		try{
-			
-			// TODO Ouput message so User knows how to leave
-			// TODO Keep message at the top of the screen at all times
+    public void start()
+    {
+        try{
             
-			//Start Looking for Messages to Receive
+            // TODO Ouput message so User knows how to leave
+            // TODO Keep message at the top of the screen at all times
+            
+            //Start Looking for Messages to Receive
             this.open();
-			//Loop for sending Messages
-			Scanner s = new Scanner(System.in);
+            //Loop for sending Messages
+            Scanner s = new Scanner(System.in);
 
-			while(true)
-			{
-				String message = s.nextLine().toLowerCase();
-				if(message.toLowerCase().equals(quittingKey))
-					break;
-				sendMessage(message);
-			}
+            while(true)
+            {
+                String message = s.nextLine().toLowerCase();
+                if(message.toLowerCase().equals(quittingKey))
+                    break;
+                sendMessage(messagePrefix + message);
+            }
             //Close all Open Streams and Desconstruct Method
             s.close();
-			this.halt();
-		}catch(Exception e)
-		{
-			e.printStackTrace();
-		}
-	}
+            this.halt();
+        }catch(Exception e)
+        {
+            e.printStackTrace();
+        }
+    }
 
     public void open()   {
         ReceiveMessages receiveThread = new ReceiveMessages(ms);
         receiveThread.start();
     }
-	public void halt()
-	{
-		ms.close();
-		receiveThread.halt();
-	}
+    public void halt()
+    {
+        ms.close();
+        receiveThread.halt();
+    }
 
-	public void sendMessage(String messageContents) throws IOException
-	{
-		//Create Packet
-		DatagramPacket msgPacket = new DatagramPacket(messageContents.getBytes(), messageContents.length(),
-		 	multicastAddress, portNum);
-		//Send Packet
-		ms.send(msgPacket);
-	}
-	
-	// TODO: Name more relevant	
-	protected class ReceiveMessages extends Thread
-	{
-		protected boolean loop = true;
-		protected MulticastSocket ms;
-
-		public ReceiveMessages(MulticastSocket ms)
-		{
-			this.ms = ms;
-		}
-        private DatagramPacket checkMessage() throws IOException
+    public void sendMessage(String messageContents) throws IOException
+    {
+        //Create Packet
+        DatagramPacket msgPacket = new DatagramPacket(messageContents.getBytes(), messageContents.length(),
+             multicastAddress, portNum);
+        //Send Packet
+        ms.send(msgPacket);
+    }
+    
+    // TODO: Name more relevant    
+    protected class ReceiveMessages extends Thread
+    {
+        protected boolean loop = true;
+        protected MulticastSocket ms;
+        protected DateTimeFormatter messageTimeFormatter;
+        public ReceiveMessages(MulticastSocket ms)
         {
-        	byte[] buf = new byte[1000];
- 			DatagramPacket recv = new DatagramPacket(buf, buf.length);
- 			ms.receive(recv);	//Blocks Here
- 			return recv;
+            this.ms = ms;
         }
-        private String processPacket(DatagramPacket packet)
+        private DatagramPacket receivePacket() throws IOException
         {
-        	//Get Packet Data and String Address
-        	byte[] packetData = packet.getData();
-        	InetAddress originAddress = packet.getAddress();
-        	//Create String
-        	String outputString = String.format("%s : %s", originAddress.getHostAddress(), (new String(packetData, StandardCharsets.UTF_8)));
-        	return outputString;
+            byte[] buf = new byte[1000];
+             DatagramPacket recv = new DatagramPacket(buf, buf.length);
+             messageTimeFormatter = DateTimeFormatter.ofPattern("HH:mm:ss");  
+             ms.receive(recv);    //Blocks Here
+             return recv;
+        }
+        private String buildMessageFromPacket(DatagramPacket packet)
+        {
+            //Get Packet Data and String Address
+            byte[] packetData = packet.getData();
+            InetAddress originAddress = packet.getAddress();
+            //Create String
+            LocalDateTime now = LocalDateTime.now();
+            String outputString = String.format("%s : %s", messageTimeFormatter.format(now), new String(packetData, StandardCharsets.UTF_8));
+            return outputString;
         }
         private void halt()
         {
-        	loop = false;
+            loop = false;
         }
         public void run()
         {
-        	try{
-	        	while(loop)
-	        	{
-	        		DatagramPacket dp = this.checkMessage();
-	        		String outputString = this.processPacket(dp);
-	        		System.out.println(outputString);
-	        	}
-	        }catch(Exception e)
-	        {
-	        	e.printStackTrace();
-	        }
+            try{
+                while(loop)
+                {
+                    DatagramPacket dp = this.receivePacket();
+                    String outputString = this.buildMessageFromPacket(dp);
+                    System.out.println(outputString);
+                }
+            }catch(Exception e)
+            {
+                e.printStackTrace();
+            }
         }
-	}
+    }
 
 }
