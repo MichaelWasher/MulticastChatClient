6f17e92322240d57afcf0470a2db5b955c737d3e
Michael Washer <michael.washer@icloud.com>
2020-06-01 21:50:35 +1200

    ChatClient has basic functionality and comments

---
+++ ChatClient.java
@@ -0,0 +1,146 @@
+/**
+ * 
+ * This application is a CLI tool for 
+ * decentralised chat using Multicast communication.
+ * I have commented this code to hopefully provide future
+ * developers with an understanding of how Multicast-IP
+ * communications can be achieved in Java.
+ *
+ * @author  Michael Washer
+ * @since   2014
+ * 
+*/
+
+
+import java.net.*;
+import java.lang.Thread;
+import java.util.Scanner;
+import java.nio.charset.StandardCharsets;
+import java.io.*;
+
+class ChatClient{
+	public static void main(String[] args)
+	{
+		Client client = new Client();
+		client.start();
+	}
+}
+
+class Client{
+	
+	MulticastSocket ms;
+	int portNum = 40202;
+	String addressString = "239.0.202.1"; ///Change Later
+	InetAddress multicastAddress;
+	ReceiveMessages receiveThread;
+
+	String quittingKey = "#quit";
+	public Client()
+	{
+		//Set Up Client
+		constructor();
+	}
+
+	private void constructor()
+	{
+		try{
+			//Set Up Client
+			multicastAddress = InetAddress.getByName(addressString);
+			ms = new MulticastSocket(portNum);
+			ms.joinGroup(multicastAddress);	
+		}catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+	}
+
+	public void start()
+	{
+		if(multicastAddress == null || ms == null)
+		{
+			constructor();
+		}
+		try{
+			//Start Looking for Messages to Receive
+			ReceiveMessages receiveThread = new ReceiveMessages(ms);
+			receiveThread.start();
+			//Loop for sending Messages
+			Scanner s = new Scanner(System.in);
+
+			while(true)
+			{
+				String message = s.nextLine().toLowerCase();
+				if(message.toLowerCase().equals(quittingKey))
+					break;
+				sendMessage(message);
+			}
+			//Close all Open Streams and Desconstruct Method
+			halt();
+		}catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+	}
+
+	public void halt()
+	{
+		ms.close();
+		receiveThread.halt();
+	}
+
+	public void sendMessage(String messageContents) throws IOException
+	{
+		//Create Packet
+		DatagramPacket msgPacket = new DatagramPacket(messageContents.getBytes(), messageContents.length(),
+		 	multicastAddress, portNum);
+		//Send Packet
+		ms.send(msgPacket);
+	}
+	
+	protected class ReceiveMessages extends Thread
+	{
+		protected boolean loop = true;
+		protected MulticastSocket ms;
+
+		public ReceiveMessages(MulticastSocket ms)
+		{
+			this.ms = ms;
+		}
+        private DatagramPacket checkMessage() throws IOException
+        {
+        	byte[] buf = new byte[1000];
+ 			DatagramPacket recv = new DatagramPacket(buf, buf.length);
+ 			ms.receive(recv);	//Blocks Here
+ 			return recv;
+        }
+        private String processPacket(DatagramPacket packet)
+        {
+        	//Get Packet Data and String Address
+        	byte[] packetData = packet.getData();
+        	InetAddress originAddress = packet.getAddress();
+        	//Create String
+        	String outputString = String.format("%s : %s", originAddress.getHostAddress(), (new String(packetData, StandardCharsets.UTF_8)));
+        	return outputString;
+        }
+        private void halt()
+        {
+        	loop = false;
+        }
+        public void run()
+        {
+        	try{
+	        	while(loop)
+	        	{
+	        		DatagramPacket dp = this.checkMessage();
+	        		String outputString = this.processPacket(dp);
+	        		System.out.println(outputString);
+	        	}
+	        }catch(Exception e)
+	        {
+	        	e.printStackTrace();
+	        }
+        }
+	}
+
+}
+
